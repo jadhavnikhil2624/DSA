2f22e311b40ecd4d2bccce4312e105fcf547c502
Nikhil Jadhav <102472043+jadhavnikhil2624@users.noreply.github.com>
2023-07-15 21:28:56 +0530

    Commit

---
+++ DeleteMiddleElementOfStack.java
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+//Initial template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int t = sc.nextInt();
+
+    while (t-- > 0) {
+      int sizeOfStack = sc.nextInt();
+      Stack<Integer> myStack = new Stack<>();
+
+      //adding elements to the stack
+      for (int i = 0; i < sizeOfStack; i++) {
+        int x = sc.nextInt();
+        myStack.push(x);
+      }
+      Solution obj = new Solution();
+      obj.deleteMid(myStack, sizeOfStack);
+
+      while (!myStack.isEmpty()) {
+        System.out.print(myStack.peek() + " ");
+        myStack.pop();
+      }
+      System.out.println();
+    }
+  }
+}
+
+// } Driver Code Ends
+
+//User function Template for Java
+
+class Solution {
+
+  public void deleteMid(Stack<Integer> s, int sizeOfStack) {
+    int index = sizeOfStack / 2;
+    if (sizeOfStack % 2 == 0) index -= 1;
+    s.remove(index);
+  }
+}
