b59e8bc366e9e8cfd230065d3539c0776e0e31e3
Nikhil Jadhav <102472043+jadhavnikhil2624@users.noreply.github.com>
2023-07-17 21:52:30 +0530

    Commit

--- QueueReversal.java
+++ QueueReversal.java
@@ -2,7 +2,7 @@
 import java.io.*;
 import java.util.*;
 
-class Reversing {
+class QueueReversal {
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
