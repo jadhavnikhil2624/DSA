6dbaea2c0addf2e923e18da374a267f025d3c95a
Nikhil Jadhav <102472043+jadhavnikhil2624@users.noreply.github.com>
2023-08-28 20:05:36 +0530

    Commit

---
+++ ImplementStackUsingQueue.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class MyStack {
+    private Queue<Integer> q;
+
+    public MyStack() {
+        q = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        q.add(x);
+        for (int i = 1; i < q.size(); i++) {
+            q.add(q.remove());
+        }
+    }
+
+    public int pop() {
+        return q.remove();
+    }
+
+    public int top() {
+        return q.peek();
+    }
+
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}
