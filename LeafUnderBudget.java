5c60f9f105a3df33ef1cf6c42f96ead9dbd0c4b9
Nikhil Jadhav <102472043+jadhavnikhil2624@users.noreply.github.com>
2023-09-02 22:11:11 +0530

    Commit

---
+++ LeafUnderBudget.java
@@ -0,0 +1,162 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+import java.lang.*; 
+
+
+class Node {
+    int data;
+    Node left, right;
+    
+    public Node(int data){
+        this.data = data;
+    }
+}
+
+class GFG {
+    
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    public static void inorder(Node root){
+        if(root == null)
+            return;
+        
+        inorder(root.left);
+        System.out.print(root.data+" ");
+        inorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t=Integer.parseInt(br.readLine());
+		while(t-- > 0){
+		    String s = br.readLine();
+	    	Node root = buildTree(s);
+	    	int k = Integer.parseInt(br.readLine());
+		    Solution obj = new Solution();
+		    int res = obj.getCount(root,k);
+		    System.out.println(res);
+		}
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+class Node {
+    int data;
+    Node left, right;
+    
+    public Node(int data){
+        this.data = data;
+    }
+}
+*/
+class Solution {
+    List<Integer> ans = new ArrayList<>();
+    int cost = 0;
+
+    public int getCount(Node node, int bud) {
+        postorder(node, bud);
+        Collections.sort(ans);
+        int sum = 0;
+        int i = 0;
+        while (i < ans.size()) {
+            sum += ans.get(i);
+            if (sum > bud) {
+                break;
+            }
+            i++;
+        }
+        return i;
+    }
+
+    private void postorder(Node node, int bud) {
+        if (node == null) {
+            return;
+        }
+        cost = cost + 1;
+        postorder(node.left, bud);
+        postorder(node.right, bud);
+        if (node.left == null && node.right == null) {
+            ans.add(cost);
+        }
+        cost = cost - 1;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        // Construct your binary tree
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        int bud = 5;
+        int result = solution.getCount(root, bud);
+        System.out.println("Result: " + result);
+    }
+}
