299e5905e4cf727ab128382b8676138cc6f957d5
Nikhil Jadhav <102472043+jadhavnikhil2624@users.noreply.github.com>
2023-09-17 14:30:05 +0530

    Commit

---
+++ PrintNFibonacciSeries.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+class GFG {
+	public static void main (String[] args) {
+	    
+	    //taking input using Scanner class
+		Scanner sc=new Scanner(System.in);
+		
+		//taking total count of testcases
+		int t=sc.nextInt();
+		
+		while(t-->0)
+		{
+		    //taking total number of elements
+		    int n=sc.nextInt();
+		    
+		    //calling printFibb() method
+		    long[] res = new Solution().printFibb(n);
+		    
+		    //printing the elements of the array
+		    for (int i = 0; i < res.length; i++)
+		        System.out.print (res[i]+" ");
+		    System.out.println();
+		}
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution
+{
+    //Function to return list containing first n fibonacci numbers.
+    public static long[] printFibb(int n) 
+    {
+        //Your code here
+        // List<Long> list = new ArrayList<>();
+        long arr[] = new long[n];
+        long p = 0, q =1,result;
+        // arr[0]=p;
+        arr[0]= q;
+        
+        for(int i=1;i<n;i++){
+            result = p+q;
+            arr[i] = result;
+            p=q;
+            q=result;
+        }
+        return arr;
+    }
+}
