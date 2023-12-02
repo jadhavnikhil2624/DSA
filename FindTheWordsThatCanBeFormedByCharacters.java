96fef338af2b8da4114600337c875ec767186c53
Nikhil Jadhav <102472043+jadhavnikhil2624@users.noreply.github.com>
2023-12-02 20:49:42 +0530

    Commit

--- FindTheWordsThatCanBeFormedByCharacters.java
+++ FindTheWordsThatCanBeFormedByCharacters.java
@@ -1,36 +1,37 @@
 class Solution {
-    public int countCharacters(String[] words, String chars) {
-        int arr[] = new int[26];
 
-        for(int i=0;i<chars.length();i++){
-            arr[chars.charAt(i)-'a']++;
-        }//End for 
+  public int countCharacters(String[] words, String chars) {
+    int arr[] = new int[26];
 
-        boolean c = false;
+    for (int i = 0; i < chars.length(); i++) {
+      arr[chars.charAt(i) - 'a']++;
+    } //End for
 
-        int sum =0;
+    boolean c = false;
 
-        for(int i=0;i<words.length;i++){
-            String temp = words[i];
-            HashMap<Character,Integer> map = new HashMap();
+    int sum = 0;
 
-            for(int j=0;j<temp.length;j++){
-                map.put(temp.charAt(j),map.getOrDefault(temp.charAt(j),0)+1)
-            }//End for j
+    for (int i = 0; i < words.length; i++) {
+      String temp = words[i];
+      HashMap<Character, Integer> map = new HashMap();
 
-            for(int k=0;k<temp.length();k++){
-                if(!arr[temp.charAt(k)-'a']>=map.get(temp.charAt(k))){
-                    c = true;
-                    break;
-                }//End if
-            }//End for k
+      for (int j = 0; j < temp.length(); j++) {
+        map.put(temp.charAt(j), map.getOrDefault(temp.charAt(j), 0) + 1);
+      } //End for j
 
-            if(!c){
-                sum +=temp.length();
-            }//End if
-            c=false;
-        }//End for i
+      for (int k = 0; k < temp.length(); k++) {
+        if (!(arr[temp.charAt(k) - 'a'] >= map.get(temp.charAt(k)))) {
+          c = true;
+          break;
+        } //End if
+      } //End for k
 
-        return sum; 
-    }//End method
-}//End class
+      if (!c) {
+        sum += temp.length();
+      } //End if
+      c = false;
+    } //End for i
+
+    return sum;
+  } //End method
+} //End class
